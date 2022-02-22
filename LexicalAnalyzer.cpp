8a31fc8366bed1a9ad71ff3a0660f50c129f74d4
coreyalmon <59987216+coreyalmon@users.noreply.github.com>
2022-02-22 01:22:12 -0500

    Create LexicalAnalyzer.cpp

---
+++ LexicalAnalyzer.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "LexicalAnalyzer.h"
+
+using namespace std;
+
+LexicalAnalyzer::LexicalAnalyzer(ifstream *scf)
+{
+  sourceCodeFile = scf;
+}
+
+bool LexicalAnalyzer::isEOF()
+{
+
+}
+
+char LexicalAnalyzer::getChar()
+{
+
+}
+
+void LexicalAnalyzer::readNextLine()
+{
+
+}
